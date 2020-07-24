1baeaa680fd59c0511dbcc0a8aad44d1e3fcd363
Rahul Mishra <49086503+MishraRahul7@users.noreply.github.com>
2020-07-24 20:59:01 +0530

    add api

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useDispatch } from "react";
 import {
   makeStyles,
   Typography,
   Grid,
-  TextField,
   Button,
   Paper,
   Link,
@@ -11,9 +10,19 @@ import {
 import { Icon } from "@iconify/react";
 import googleIcon from "@iconify/icons-mdi/google";
 import facebookIcon from "@iconify/icons-mdi/facebook";
+import { Formik } from "formik";
+import { TextField } from "formik-material-ui";
+import * as Yup from "yup";
 
 import NavBar from "../container/NavBar";
 
+const SignInSchema = Yup.object().shape({
+  email: Yup.string().email().required("This field is required"),
+  password: Yup.string()
+    .min(6, "Password too short")
+    .max(20, "Password too long")
+    .required("This field is required"),
+});
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
     height: "100vh",
@@ -66,6 +75,7 @@ const useStyles = makeStyles((theme) => ({
 
 const SignIn = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <div className={classes.mainContainer}>
@@ -76,97 +86,110 @@ const SignIn = () => {
             <Typography component="h1" className={classes.signInHead}>
               Sign In With
             </Typography>
-            <form className={classes.form}>
-              <Grid container direction="row" spacing={2}>
-                <Grid item xs={12} md={6} align="right">
-                  <Link href="/" className={classes.text} variant="body2">
-                    <Button
-                      variant="outlined"
-                      size="large"
-                      fullWidth
-                      className={classes.socialLoginButton}
-                    >
-                      <Icon
-                        height="2em"
-                        className={classes.icons}
-                        icon={facebookIcon}
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={SignInSchema}
+              onSubmit={(values) => {
+                dispatch(signIn(values));
+              }}
+            >
+              {({ errors, handleChange, touched }) => (
+                <Form className={classes.form}>
+                  <Grid container direction="row" spacing={2}>
+                    <Grid item xs={12} md={6} align="right">
+                      <Link href="/" className={classes.text} variant="body2">
+                        <Button
+                          variant="outlined"
+                          size="large"
+                          fullWidth
+                          className={classes.socialLoginButton}
+                        >
+                          <Icon
+                            height="2em"
+                            className={classes.icons}
+                            icon={facebookIcon}
+                          />
+                          Facebook
+                        </Button>
+                      </Link>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Link href="/" className={classes.text} variant="body2">
+                        <Button
+                          variant="outlined"
+                          size="large"
+                          fullWidth
+                          className={classes.socialLoginButton}
+                        >
+                          <Icon
+                            height="2em"
+                            className={classes.icons}
+                            icon={googleIcon}
+                          />
+                          Google
+                        </Button>
+                      </Link>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <TextField
+                        name="email"
+                        variant="outlined"
+                        fullWidth
+                        label="Email Address"
+                        type="text"
+                        autoComplete="off"
+                        InputLabelProps={{
+                          style: { color: "black" },
+                        }}
+                        className={classes.root}
                       />
-                      Facebook
-                    </Button>
-                  </Link>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <Link href="/" className={classes.text} variant="body2">
-                    <Button
-                      variant="outlined"
-                      size="large"
-                      fullWidth
-                      className={classes.socialLoginButton}
-                    >
-                      <Icon
-                        height="2em"
-                        className={classes.icons}
-                        icon={googleIcon}
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <TextField
+                        variant="outlined"
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        InputLabelProps={{
+                          style: { color: "black" },
+                        }}
+                        className={classes.root}
                       />
-                      Google
-                    </Button>
-                  </Link>
-                </Grid>
-                <Grid item xs={12} md={12}>
-                  <TextField
-                    name="email"
-                    variant="outlined"
-                    fullWidth
-                    label="Email Address"
-                    type="text"
-                    autoComplete="off"
-                    InputLabelProps={{
-                      style: { color: "black" },
-                    }}
-                    className={classes.root}
-                  />
-                </Grid>
-                <Grid item xs={12} md={12}>
-                  <TextField
-                    variant="outlined"
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    InputLabelProps={{
-                      style: { color: "black" },
-                    }}
-                    className={classes.root}
-                  />
-                </Grid>
-                <Grid item xs={12} md={12} align="center">
-                  <Button
-                    type="submit"
-                    fullWidth
-                    size="large"
-                    variant="contained"
-                    color="default"
-                    className={classes.signInBtn}
-                  >
-                    Sign In
-                  </Button>
-                </Grid>
-                <Grid item xs={12} md={12}>
-                  <Typography className={classes.signUpLink} align="center">
-                    OR
-                  </Typography>
-                </Grid>
-                <Grid item xs={12} md={12} align="center">
-                  <Link
-                    to="/registration"
-                    variant="body2"
-                    className={classes.signUpLink}
-                  >
-                    Not a Donor? Sign Up Now
-                  </Link>
-                </Grid>
-              </Grid>
-            </form>
+                    </Grid>
+                    <Grid item xs={12} md={12} align="center">
+                      <Button
+                        type="submit"
+                        fullWidth
+                        size="large"
+                        variant="contained"
+                        color="default"
+                        className={classes.signInBtn}
+                      >
+                        Sign In
+                      </Button>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <Typography className={classes.signUpLink} align="center">
+                        OR
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={12} align="center">
+                      <Link
+                        to="/registration"
+                        variant="body2"
+                        className={classes.signUpLink}
+                      >
+                        Not a Donor? Sign Up Now
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
           </Paper>
         </Grid>
       </Grid>
