9c8039f32fd9e5bd38bc7e6ea6d2a6b58bcbced9
Rahul Mishra <49086503+MishraRahul7@users.noreply.github.com>
2020-07-31 22:28:39 +0530

    commit

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: "https://madadbackend.herokuapp.com",
-=======
   baseURL: " https://madadbackend.herokuapp.com",
   // baseURL: "http://localhost:8000",
->>>>>>> updateCode
 });
